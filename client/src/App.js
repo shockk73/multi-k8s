18194cb29d3cfca5d70d6c750dc77c18ace55e46
shockk73 <shockk73@gmail.com>
2022-10-25 14:19:39 +0300

    added k8s

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ function App() {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Fib Calculator!
+            Fib Calculator version 2!
           </p>
           <a
             className="App-link"
